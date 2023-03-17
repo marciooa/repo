463dcd3400af62e079735799147070f10ab0b3b2
marciooa <66385642+marciooa@users.noreply.github.com>
2023-03-17 11:09:19 -0300

    yes

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.set_page_config(layout="wide", page_title='QuantMind', page_icon='favicon.ico
 
 st.image("logo.png", width=150)
 
-st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
+st.markdown('<style>' + open('style.css').read() + '</style>', unsafe_allow_html=True)
 
 ##################### autenticacao
 def check_password():
